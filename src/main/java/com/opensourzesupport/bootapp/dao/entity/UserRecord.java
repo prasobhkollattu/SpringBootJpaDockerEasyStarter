0e3806029b1b48f55ce58c82da59153fd19c313f
prasobhk <prasobh@obtil.com>
2019-07-25 20:09:24 +0200

    lombok added .. no mere gettre and setters ...

--- src/main/java/com/opensourzesupport/bootapp/dao/entity/UserRecord.java
+++ src/main/java/com/opensourzesupport/bootapp/dao/entity/UserRecord.java
@@ -5,7 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class UserRecord {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,36 +16,4 @@ public class UserRecord {
 	private String name;
 	private String email;
 
-	public UserRecord() {
-	}
-
-
-
-	public int getId() {
-		return id;
-	}
-
-
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 }
